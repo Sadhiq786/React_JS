d5eb0b42c1b9bdb2a1760d405cef307df204f50f
Shaik Sadhiq <sksadhiqsksadhiq@gmail.com>
2024-02-26 18:41:37 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import TableItems from './Components/class-components/Products/Tabledata';
+import CircleGenerator from './Components/class-components/Products/circlesAdd';
 import Products from './Components/class-components/Products/products';
 
 function App() {
   return (
     <div>
-      <Products/>
+      {/* <Products/> */}
+
+      <TableItems/>
     </div>
   );
 }
