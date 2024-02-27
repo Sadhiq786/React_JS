3770e4ba055ec41a3cbdef5e75ed3d6b8f1caa45
Shaik Sadhiq <sksadhiqsksadhiq@gmail.com>
2024-02-27 22:26:07 +0530

    Added

--- src/Components/class-components/CarouselTask/CarouselTask.js
+++ src/Components/class-components/CarouselTask/CarouselTask.js
@@ -1,20 +1,40 @@
 import { Component } from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import TableImage from "../../images/img";
 
 class CarouselTask extends Component{
-    constructor()
-    {
-        super()
+    state = {
+        carousel:[]
     }
+
+    addSlides=()=>{
+        const slides={
+            id: this.state.carousel.length + 1,
+            text : `carousel ${this.state.carousel.length + 1}`,
+            imageUrl:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        }
+
+        this.setState((prevState)=>({
+            carousel:[...prevState.carousel,slides],
+        }))}
+
         render()
         {
             return(
-                <div>
+                <>
+                    <button onClick={this.addSlides}>Add Slides</button>
                     <Carousel>
-                       <img src={"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"}></img>
+                        {
+                            this.state.carousel.map(slide=>(
+                                <Carousel.Item key = {slide.id}>
+                                    <img src={slide.imageUrl} style={{width:"100%"}}></img>
+                                    <Carousel.Caption>
+                                        <h2>{slide.text}</h2>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            ))
+                        }
                     </Carousel>
-                </div>
+                </>
             )
         }
     }
